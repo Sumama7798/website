71ac5d2a0eeb6a373d054a3501bf0b335106ce79
Sumama <sumamabutt500@gmial.com>
2024-11-14 15:27:08 +0500

    navbar added

---
+++ admin-panel/src/components/Navbar/Navbar.content.jsx
@@ -0,0 +1,62 @@
+// NavbarContent.js
+import React from 'react';
+import { FolderDot, LogOut } from 'lucide-react';
+import { Link } from 'react-router-dom';
+
+const NavbarContent = ({ toggleDropdown, isDropdownOpen, closeDropdown }) => {
+  return (
+    <header className='navbar'>
+      <div className='container'>
+        <Link to='/' className='logo'>
+          <FolderDot className='w-8 h-8 font-bold' />
+          <span>Admin Panel</span>
+        </Link>
+
+        <nav className='flex items-center space-x-4'>
+          {/* Dropdown component */}
+          <div className='relative inline-block text-left'>
+            <button
+              onClick={toggleDropdown}
+              className='dropdown-btn'
+            >
+              Options↓
+            </button>
+
+            {isDropdownOpen && (
+              <div
+                className='dropdown-menu'
+                onMouseLeave={closeDropdown} // Optional: closes when mouse leaves
+              >
+                <ul className='py-1'>
+                  <li>
+                    <a href='#' className='inline-flex px-4 py-2 text-sm hover:bg-emerald-600'>
+                      Option 1
+                    </a>
+                  </li>
+                  <li>
+                    <a href='#' className='inline-flex px-4 py-2 text-sm hover:bg-emerald-600'>
+                      Option 2
+                    </a>
+                  </li>
+                  <li>
+                    <a href='#' className='inline-flex px-4 py-2 text-sm hover:bg-emerald-600'>
+                      Option 3
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+
+          {/* Log Out button */}
+          <Link to='/login' className='logout-btn'>
+            <LogOut className='w-6 h-6 text-red-500 hover:text-emerald-400' />
+            
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default NavbarContent;
---
+++ admin-panel/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+// Navbar.js
+import React, { useState } from 'react';
+import NavbarContent from './Navbar.content.jsx'
+import './Navbar.css'; // Import the custom CSS file
+
+const Navbar = () => {
+  // State for handling dropdown visibility
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const closeDropdown = () => {
+    setIsDropdownOpen(false);
+  };
+
+  return (
+    <NavbarContent
+      toggleDropdown={toggleDropdown}
+      isDropdownOpen={isDropdownOpen}
+      closeDropdown={closeDropdown}
+    />
+  );
+};
+
+export default Navbar;
